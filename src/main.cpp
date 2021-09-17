04a25b1bd1556af13ea97b4ecf05e4568d65ec28
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-09-16 23:11:22 -0400

    [test] try Ogre3D Bootstrap tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,9 @@
 #include <iostream>
 
 
-#include <GLFW/glfw3.h>
-
 
 int main()
 {
-
-    if (!glfwInit())
-    {
-        // Initialization failed
-    }
-
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    if (!window)
-    {
-        // Window or OpenGL context creation failed
-    }
 
     std::cout << "lala" << std::endl;
     return 0;
