cf8bc5b4b385226c0571d9db00ed39238562bfc3
CelestinML <celestin.moenne@gmail.com>
2021-10-18 07:51:55 -0400

    Auto stash before merge of "dev/implement-force-generator" and "origin/dev/trainee+bungeespring"

--- src/test.cpp
+++ src/test.cpp
@@ -5,48 +5,11 @@
 #include "PhysicsEngine/Fireball.hpp"
 #include "PhysicsEngine/Laser.hpp"
 #include "PhysicsEngine/Bullet.hpp"
+#include "PhysicsEngine/ParticleAnchoredSpring.hpp"
+#include "PhysicsEngine/ParticleBuoyancy.hpp"
 
 int main()
 {
-    int nbFireballs = 1;
-    int nbLasers = 1;
-    int nbBullets = 1;
-    std::vector<Projectile *> projectiles;
+    ParticleForceRegistry registry = ParticleForceRegistry();
 
-    for (int i = 0; i < nbFireballs; i++) {
-        projectiles.push_back(new Fireball(Vector3D(0, 100, 0), Vector3D(1, 0, 0), 6, 1));
-    }
-    for (int i = 0; i < nbLasers; i++) {
-        projectiles.push_back(new Laser(Vector3D(0, 100, 0), Vector3D(1, 0, 0), 4, 1));
-    }
-    for (int i = 0; i < nbBullets; i++) {
-        projectiles.push_back(new Bullet(Vector3D(0, 100, 0), Vector3D(1, 0, 0), 2, 1));
-    }
-
-    for (Projectile* projectile : projectiles) {
-        std::cout << "Speed factor : " << projectile->getSpeedFactor() << std::endl;
-        std::cout << "Initial velocity : " << projectile->getVelocity() << std::endl;
-        std::cout << "Force : " << projectile->getForces()[0] << std::endl;
-    }
-
-    Scene scene = Scene(projectiles);
-
-    GameEngine gameEngine = GameEngine(scene);
-
-    gameEngine.run();
-
-    float simulationTime = 10;
-    float time = static_cast<float>(clock()) / CLOCKS_PER_SEC;
-    float simulationEndTime = time + simulationTime;
-
-    while (time < simulationEndTime) {
-        std::vector<Particle> particles = gameEngine.getParticles();
-        for (Particle particle : particles) {
-            std::cout << particle << std::endl;
-        }
-        Sleep(1000);
-        time = static_cast<float>(clock()) / CLOCKS_PER_SEC;
-    }
-
-    gameEngine.stop();
 }
