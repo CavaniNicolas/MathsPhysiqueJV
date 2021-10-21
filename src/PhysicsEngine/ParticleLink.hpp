49704bf0dd4763476475e023b545cd6704091c8e
CelestinML <celestin.moenne@gmail.com>
2021-10-20 21:06:50 -0400

    [dev] create and define ParticleLink class

---
+++ src/PhysicsEngine/ParticleLink.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ParticleContactGenerator.hpp"
+
+class ParticleLink : public ParticleContactGenerator
+{
+  private:
+	// Linked particles
+    std::vector<std::shared_ptr<Particle>> m_particle;
+
+  public:
+
+    ParticleLink(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB);
+    ~ParticleLink();
+
+	// return the length of the link
+    float currentLength() const;
+
+	// Take a pointer to an array of contact and the number of contact left to be generated
+    unsigned int addContact(std::shared_ptr<std::vector<std::shared_ptr<ParticleContact>>> contacts,
+                            unsigned int limit) const = 0;
+};
