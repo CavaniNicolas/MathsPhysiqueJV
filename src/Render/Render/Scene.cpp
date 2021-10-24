cf64c45fd19c1a3bd530d7b62d151d9a518c5080
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-10-23 23:14:51 -0400

    [dev] implement render::Scene functionalities

--- src/Render/Render/Scene.cpp
+++ src/Render/Render/Scene.cpp
@@ -6,4 +6,41 @@ namespace render
 
 Scene::Scene(Camera& camera): m_camera(camera) {}
 
+void Scene::update(Window& window)
+{
+    updateRenderedMeshes();
+    updateCamera(window);
+}
+
+void Scene::updateCamera(Window& window)
+{
+    // handle inputs to move the camera
+    m_camera.handleInputs(window);
+
+    // fix the camera dimensions if the window gets resized
+    {
+        int width, height;
+        glfwGetWindowSize(window.getWindow(), &width, &height);
+        // Update the camera matrices view and proj
+        m_camera.setSize(width, height);
+        glViewport(0, 0, width, height);
+    }
+
+    // Update the camera matrices view and proj
+    m_camera.update(0.1f, 10000.0f);
+}
+
+void Scene::updateRenderedMeshes()
+{
+    for(auto& renderedMesh: m_renderedMeshes)
+    {
+        renderedMesh->updateModelMatrix();
+    }
+}
+
+void Scene::addRenderedMesh(std::shared_ptr<RenderedMesh> renderedMesh)
+{
+    m_renderedMeshes.push_back(renderedMesh);
+}
+
 } // namespace render
--- src/Render/Render/Scene.hpp
+++ src/Render/Render/Scene.hpp
@@ -1,26 +1,34 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 
 #include "Render/Camera.hpp"
 #include "Render/RenderedMesh.hpp"
+#include "Render/Window.hpp"
 
 namespace render
 {
 
 class Scene
 {
-  private:
-    std::vector<RenderedMesh> m_renderedMeshes;
-    Camera m_camera;
-
   public:
     Scene(Camera& camera = Camera(960, 540, glm::vec3(0.0f, 15.0f, 80.0f)));
+
+    void update(Window& window);
+    void addRenderedMesh(std::shared_ptr<RenderedMesh> renderedMesh);
 
     Camera& getCamera()
     {
         return m_camera;
     }
+
+  private:
+    std::vector<std::shared_ptr<RenderedMesh>> m_renderedMeshes;
+    Camera m_camera;
+
+    void updateCamera(Window& window);
+    void updateRenderedMeshes();
 };
 
 } // namespace render
