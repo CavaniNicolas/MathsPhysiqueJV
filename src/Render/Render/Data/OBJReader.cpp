80fd4e86774b570254e16f0338376663ae16dad8
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-11-10 00:33:49 -0500

    [dev] read vertices and indices from obj file

--- src/Render/Render/Data/OBJReader.cpp
+++ src/Render/Render/Data/OBJReader.cpp
@@ -29,11 +29,75 @@ render::Mesh OBJReader::readOBJFromFile(const std::string& filename)
 
     while(!file.eof())
     {
-        file.getline(line, 256);
-        strstream << line;
-    }
+        //        file.getline(line, 256);
+        //        strstream << line;
 
-    //    std::cout << strstream.str() << std::endl;
+        // get first word of the line
+        file >> line;
+
+        // if its a comment, skip it
+        if(!std::strcmp(line, "#"))
+        {
+            file.getline(line, 256);
+        }
+        // if its a vertex
+        else if(!std::strcmp(line, "v"))
+        {
+            float x, y, z;
+            file >> x >> y >> z;
+
+            //            std::cout << "x: " << x << ", y: " << y << ", z: " << z << std::endl;
+            vertices.push_back(Vertex{glm::vec3(x, y, z), glm::vec3(0, 0, 0)});
+        }
+        // if its a texture vertex
+        else if(!std::strcmp(line, "vt"))
+        {
+            float u, v;
+            file >> u >> v;
+
+            //            std::cout << "u: " << u << ", v: " << v << std::endl;
+            // TO DO, save it
+        }
+        // if its a normal vertex
+        else if(!std::strcmp(line, "vn"))
+        {
+            float nx, ny, nz;
+            file >> nx >> ny >> nz;
+
+            //            std::cout << "nx: " << nx << ", ny: " << ny << ", nz: " << nz << std::endl;
+            // TO DO, save it
+        }
+        // if its an index
+        else if(!std::strcmp(line, "f"))
+        {
+            std::string l;
+            unsigned int v[3], vt[3], vn[3];
+
+            auto slash = std::string::npos;
+            for(int i = 0; i < 3; ++i)
+            {
+                file >> l;
+                v[i] = std::stoi(l);
+
+                slash = l.find("/");
+                l.erase(0, slash + 1);
+
+                vt[i] = std::stoi(l);
+
+                slash = l.find("/");
+                l.erase(0, slash + 1);
+
+                vn[i] = std::stoi(l);
+            }
+
+            //            std::cout << "v[0]: " << v[0] << ", vt[0]: " << vt[0] << ", vn[0]: " << vn[0] << std::endl;
+
+            // - 1 to every vertex because they start at 1 in the file
+            indices.push_back(--v[0]);
+            indices.push_back(--v[1]);
+            indices.push_back(--v[2]);
+        }
+    }
 
     return Mesh(vertices, indices);
 }
