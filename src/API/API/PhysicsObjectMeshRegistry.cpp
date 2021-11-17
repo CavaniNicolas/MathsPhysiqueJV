630b9ddddfdd772739e7391aa80812115f0fb4fb
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-11-17 00:40:17 -0500

    [dev] start passing the transform matrix from engine to render

--- src/API/API/PhysicsObjectMeshRegistry.cpp
+++ src/API/API/PhysicsObjectMeshRegistry.cpp
@@ -31,9 +31,16 @@ void PhysicsObjectMeshRegistry::updateMeshMatrixFromPhysicsObject()
         else if(std::shared_ptr<engine::RigidBody> rigidBody =
                   std::dynamic_pointer_cast<engine::RigidBody>(physicsObject))
         {
-            //            // get the Matrix34 from the engine but make it a glm::mat4
-            //            glm::mat4 matrix = {}
-            //            renderedMesh->setModelMatrix(matrix);
+            // get the Matrix34 from the engine but make it a glm::mat4
+            std::array<std::array<float, 4>, 3> values = rigidBody->getTransformationMatrix().getValuesArray();
+
+            glm::mat4 matrix({{values[0][0], values[0][1], values[0][2], values[0][3]},
+                              {values[1][0], values[1][1], values[1][2], values[1][3]},
+                              {values[2][0], values[2][1], values[2][2], values[2][3]},
+                              {0, 0, 0, 1}});
+            //            std::cout << matrix[0][0] << matrix[0][1] << matrix[0][2] << matrix[0][3] << std::endl;
+
+            renderedMesh->setModelMatrix(matrix);
         }
     }
 }
